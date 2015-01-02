8f6e23b9b26ecc8c05467abbc32102bf322bc4b0
GrishdaFish <demonichacksaw666@gmail.com>
2015-01-01 22:53:32 -0500

    Stuff

--- staticfy.py
+++ staticfy.py
@@ -22,8 +22,10 @@ def fix_file(filename):     # give it the name of the file
     src_found = 0               # holds index of current change or -1 for EOF
     href_found = 0              # holds index of current change or -1 for EOF
     done = 0                    # holds -1 when both src and href checks reach EOF
-    link_open = '\"{% static '
-    link_close = ' %}\"'
+    LINK_OPEN = '\"{% static '
+    LINK_CLOSE = ' %}\"'
+    STATIC_LEN_OPEN = len(LINK_OPEN)+1
+
     while done != -1:           # while not EOF look for any instance of 'src=' or 'href='
         while src_found != -1:                                      # if prev search didn't reach EOF
             src_found = file_contents.find("src=", src_found)       # find an instance of src
@@ -31,11 +33,11 @@ def fix_file(filename):     # give it the name of the file
                 src_found += 4                                      # position after =
             else:
                 break
-            new_contents = file_contents[:src_found] + link_open + file_contents[src_found:]  # append "{% static
+            new_contents = file_contents[:src_found] + LINK_OPEN + file_contents[src_found:]  # append "{% static
             file_contents = new_contents                            # retain new contents
-            quotes = file_contents.find("\"", (src_found + (len(link_open)+1)))     # find the next available "
+            quotes = file_contents.find("\"", (src_found + STATIC_LEN_OPEN))     # find the next available "
             quotes += 1                                             # move to next spot for append
-            new_contents = file_contents[:quotes] + link_close + file_contents[quotes:]  # append ' %}"'
+            new_contents = file_contents[:quotes] + LINK_CLOSE + file_contents[quotes:]  # append ' %}"'
             file_contents = new_contents                            # write to new string
         while href_found != -1:                                     # if prev search didn't reach EOF
             href_found = file_contents.find("href=", href_found)    # find an instance of href
@@ -43,11 +45,11 @@ def fix_file(filename):     # give it the name of the file
                 href_found += 5                                     # position after =
             else:
                 break
-            new_contents = file_contents[:href_found] + link_open + file_contents[href_found:]  # append "{% static
+            new_contents = file_contents[:href_found] + LINK_OPEN + file_contents[href_found:]  # append "{% static
             file_contents = new_contents                            # retain new contents
-            quotes = file_contents.find("\"", (href_found + (len(link_open)+1)))    # find the next available whitespace
+            quotes = file_contents.find("\"", (href_found + STATIC_LEN_OPEN))    # find the next available whitespace
             quotes += 1                                             # move to next spot for append
-            new_contents = file_contents[:quotes] + link_close + file_contents[quotes:]  # append ' %}'
+            new_contents = file_contents[:quotes] + LINK_CLOSE + file_contents[quotes:]  # append ' %}'
             file_contents = new_contents                            # write to new string
         if href_found == -1 and src_found == -1:                    # if both reach EOF kill loop
             done = -1
